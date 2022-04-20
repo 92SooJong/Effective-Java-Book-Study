264b0133c4a7e6406a720020af5f480636dd9309
92SooJong <hsz3855@gmail.com>
2022-04-20 22:47:30 +0900

    enum 연습

--- sample/src/Main.java
+++ sample/src/Main.java
@@ -6,25 +6,38 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(APPLE.FUJI);
+        System.out.println(APPLE.FUJI.getName());
+        System.out.println(APPLE.PIPPIN.getName());
         System.out.println(APPLE.PIPPIN);
 
+
     }
-    
+
 }
 
 
-class APPLE{
-    public static final APPLE FUJI = new APPLE();
-    public static final APPLE PIPPIN = new APPLE();
-}
-
-class ORANGE{
-    public static final ORANGE FUJI = new ORANGE();
-    public static final ORANGE PIPPIN = new ORANGE();
-}
-
-//enum APPLE{
-//    FUJI,PIPPIN
+//class APPLE{
+//    public static final APPLE FUJI = new APPLE();
+//    public static final APPLE PIPPIN = new APPLE();
 //}
+
+//class ORANGE{
+//    public static final ORANGE FUJI = new ORANGE();
+//    public static final ORANGE PIPPIN = new ORANGE();
+//}
+
+enum APPLE{
+    FUJI("fuji"),
+    PIPPIN("pippin");
+
+    private final String name;
+    APPLE(String a){
+        this.name = a;
+    }
+
+    String getName(){
+        return this.name;
+    }
+
+}
 
