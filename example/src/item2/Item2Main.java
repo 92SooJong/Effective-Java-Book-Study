b3801122a3361a2eb56c7147ab829116d2759741
Soojong <hsz3855@gmail.com>
2023-06-12 02:04:39 +0900

    item2 - Builder 클래스 작성

---
+++ example/src/item2/Item2Main.java
@@ -0,0 +1,18 @@
+package item2;
+
+import java.util.List;
+
+import item1.Person;
+
+/**
+ * 아이템2 : 생성자에 매개변수가 많다면 빌더를 고려하라
+ */
+public class Item2Main {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
+
